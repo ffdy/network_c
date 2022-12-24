a3ab718d3f87da93aa8133cf1a8f5c4b53922d35
ffdy <scaryffy@qq.com>
2022-12-25 02:41:34 +0800

    client signal scanf重入

--- server.c
+++ server.c
@@ -13,15 +13,21 @@
 
 int sockfd, newfd;
 struct sockaddr_in client_addr;
-socklen_t client_addr_len;
+socklen_t client_addr_len = sizeof(client_addr); // !非常重要，不然获取不到client_addr
+char buf[1024];
 
 void sigio_handler(int signo) {
-  char buf[1024];
+  sleep(5);
   bzero(buf, sizeof buf);
   while (-1 !=
-      recvfrom(sockfd, buf, sizeof buf, 0, (struct sockaddr *)&client_addr, &client_addr_len)) {
+      recvfrom(sockfd, buf, sizeof(buf), 0, (struct sockaddr *)&client_addr, &client_addr_len)) {
     printf("Message from %s: %s\n", inet_ntoa(client_addr.sin_addr), buf);
-    sendto(sockfd, "ACK", 3, 0, (struct sockaddr *)&client_addr, client_addr_len);
+    char send_buf[] = "ACK";
+    if (-1 == sendto(sockfd, send_buf, strlen(send_buf), 0, (struct sockaddr *)&client_addr,
+                     sizeof(client_addr))) {
+      perror("sendto");
+    }
+    printf("Send success\n");
   }
 
   perror("recvfrom");
@@ -32,7 +38,6 @@ int main(int args, char *argv[]) {
   struct sigaction action;
   bzero(&action, sizeof(struct sigaction));
   action.sa_handler = sigio_handler;
-  // action.sa_mask = SIGIO;
   if (-1 == sigaction(SIGIO, &action, NULL)) {
     perror("sigaction");
     exit(1);
@@ -47,7 +52,7 @@ int main(int args, char *argv[]) {
     perror("fcntl get flag");
     exit(1);
   }
-  fcntl_flag |= O_NONBLOCK | O_ASYNC;
+  fcntl_flag |= O_ASYNC | O_NONBLOCK;
   if (-1 == fcntl(sockfd, F_SETFL, fcntl_flag)) {
     perror("fcntl set flag");
     exit(1);
@@ -59,6 +64,7 @@ int main(int args, char *argv[]) {
 
   struct sockaddr_in server_addr;
   socklen_t server_addr_len;
+  server_addr.sin_family = PF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
   server_addr.sin_port = htons(atoi(argv[1]));
 
